226444f7fdae63cc8d3ae4bf810810ef37d62df1
abc <samratsaha23122007@gmail.com>
2022-07-31 21:57:33 +0530

    casino website first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('icon').addEventListener('click', ()=>{
+    document.getElementById('nav').style.display="block";
+});
+document.getElementById('close').addEventListener('click', ()=>{
+    document.getElementById('nav').style.display="none";
+});
